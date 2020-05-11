3c8fd5aec4107f48ea7b112889201a8e81cbd659
Chinmaya Pulikallu <chinmayapulikallu@gmail.com>
2020-05-11 14:46:50 -0500

    updated speedcontrol component

--- src/components/SpeedControl/SpeedControl.js
+++ src/components/SpeedControl/SpeedControl.js
@@ -1,20 +1,33 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 // THIS COMPONENT IS OUR INTERFACE FOR SPEED
 // YOU SHOULD DISPLAY THE CURRENT SPEED
 // BUTTONS SHOULD INCREASE OR DECREASE SPEED, RESPECTIVELY
 
 class SpeedControl extends Component {
+
+  increaseSpeed = () => {
+    console.log('in increaseSpeed');
+    this.props.dispatch({type: 'SPEED_INCREASE', payload: 1})
+  }
+
+  decreaseSpeed = () => {
+    console.log('in decreaseSpeed');
+    this.props.dispatch({ type: 'SPEED_DECREASE', payload: 1})
+  }
+
+
   render() {
     return (
       <div>
         <h2>Speed Control</h2>
-
-        <button>Increase Speed</button>
-        <p>SPEED: GOES HERE</p>
-        <button>Decrease Speed</button>
+        <button onClick={this.increaseSpeed}>Increase Speed</button>
+        <p>SPEED:{this.props.reduxState.count} </p>
+        <button onClick={this.decreaseSpeed}>Decrease Speed</button>
       </div>
     )
   }
 }
 
-export default SpeedControl;
+const putReduxStateOnProps = (reduxState) => ({reduxState})
+export default connect(putReduxStateOnProps)(SpeedControl);
--- src/index.js
+++ src/index.js
@@ -5,9 +5,19 @@ import App from './components/App/App';
 import { createStore } from 'redux'
 import { Provider } from 'react-redux';
 
+
+
 // put your reducer here!
+const speedReducer = (state={count: 0}, action) => {
+    if (action.type === 'SPEED_INCREASE') {
+        return {count: state.count + 1}
+    } else if (action.type === 'SPEED_DECREASE') {
+        return { count: state.count - 1 }
+    }
+        return state;
+}
 
 // use reducer in store
-const storeInstance = createStore(  );
+const storeInstance = createStore(speedReducer);
 
 ReactDOM.render(<Provider store={storeInstance}><App /></Provider>, document.getElementById('root'));
